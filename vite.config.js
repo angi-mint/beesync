02dc4ffaabe1ef4c61fea931d4f7032f7cee373f
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-05 22:15:08 +0200

    vite setup
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,10 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        origin: 'https://beesync.ddev.site:5173',
+    }
 });
