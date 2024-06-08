b9548ee1898ec24b072f58f7c7ba60c04a1f474d
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 19:52:52 +0200

    change header component
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ resources/js/Layouts/BaseLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import Navigation from "@/Layouts/Header/Navigation.vue";
+import UserNavigation from "@/Layouts/Header/UserNavigation.vue";
+
+</script>
+
+<template>
+    <header>
+        <Navigation></Navigation>
+        <span>
+            <slot name="title"></slot>
+        </span>
+        <UserNavigation></UserNavigation>
+    </header>
+    <main>
+        <slot name="main"/>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
-import HeaderInc from "@/Layouts/Header/HeaderInc.vue";
+import {useI18n} from "vue-i18n";
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+const { t } = useI18n();
+
 </script>
 
 <template>
     <Head title="Home"/>
-<HeaderInc></HeaderInc>
+    <BaseLayout>
+        <template #title>{{ $t("navigation.home") }}</template>
+        <template #main>
+            <h2></h2>
+        </template>
+    </BaseLayout>
 </template>
 
 <style scoped>
