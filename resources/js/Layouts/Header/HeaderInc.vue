9ea2c27d0f4e5225515908cb03c59697e76afc56
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 15:49:58 +0200

    create header and navigation
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ resources/js/Layouts/Header/HeaderInc.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import Navigation from "@/Layouts/Header/Navigation.vue";
+</script>
+
+<template>
+    <header>
+        <Navigation></Navigation>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import HeaderInc from "@/Layouts/Header/HeaderInc.vue";
+</script>
+
+<template>
+<HeaderInc></HeaderInc>
+</template>
+
+<style scoped>
+
+</style>
