1c8f7b01015a909e61361f74a8c2aa38a50ff5c8
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 16:47:45 +0200

    added custom font
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Julius Sans One', ...defaultTheme.fontFamily.sans],
             },
         },
     },
