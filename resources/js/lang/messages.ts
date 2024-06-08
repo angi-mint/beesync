bc09214262689d31d7572d93fb2022fa9fc4f6a3
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 21:30:25 +0200

    added user, general & dashboard snippets
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- resources/js/lang/messages.ts
+++ resources/js/lang/messages.ts
@@ -1,28 +1,50 @@
 export default {
     en: {
-        header: {
-            login: 'Login',
-            logout: 'Logout',
-        },
-        navigation: {
-            home: 'Home',
+        nav: {
+            dashboard: 'Dashboard',
             tasks: 'Tasks',
             calendar: 'Calendar',
             friends: 'Friends',
             timer: 'Timer',
         },
+        user: {
+            profile: 'Profile',
+            settings: 'Settings',
+            login: 'Login',
+            logout: 'Logout',
+            register: 'Register',
+        },
+        general: {
+            welcome: 'Welcome to Beesync',
+            welcome_back: 'Welcome back, {name}!',
+        },
+        dashboard: {
+            pending_tasks: 'Pending Tasks',
+            top_three_users: 'Top 3 Users',
+        }
     },
     de : {
-        header: {
-            login: 'Anmelden',
-            logout: 'Abmelden',
-        },
-        navigation: {
-            home: 'Startseite',
+        nav: {
+            dashboard: 'Dashboard',
             tasks: 'Aufgaben',
             calendar: 'Kalender',
             friends: 'Freunde',
             timer: 'Timer',
         },
+        user: {
+            profile: 'Profil',
+            settings: 'Einstellungen',
+            login: 'Anmelden',
+            logout: 'Abmelden',
+            register: 'Registrieren',
+        },
+        general: {
+            welcome: 'Willkommen bei Beesync',
+            welcome_back: 'Willkommen zurück, {name}!',
+        },
+        dashboard: {
+            pending_tasks: 'Ausstehende Aufgaben',
+            top_three_users: 'Top 3 Benutzer',
+        }
     }
 }
