24e3ac9b867786a80f89f7a9d2a3001d4f7dd4d5
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 21:56:45 +0200

    task & timer overviews
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ resources/js/Pages/Task/TaskOverview.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import {useI18n} from "vue-i18n";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+
+const {t} = useI18n();
+</script>
+
+<template>
+    <Head title="Timer"/>
+    <AuthenticatedLayout>
+        <template #header>{{$t('nav.tasks')}}</template>
+    </AuthenticatedLayout>
+</template>
+
+<style scoped>
+
+</style>
