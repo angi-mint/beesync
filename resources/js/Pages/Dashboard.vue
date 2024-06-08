baf1e12c8e9e16b76aa34c261c710f2c964eb1b1
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 21:29:13 +0200

    changed text i18n snippets
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import {useI18n} from "vue-i18n";
+
+const {t} = useI18n();
 </script>
 
 <template>
@@ -8,13 +11,15 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{$t("nav.dashboard")}}</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <h2>{{$t("general.welcome_back", {name: $page.props.auth.user.name})}}</h2>
+
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">{{$t("dashboard.pending_tasks")}}</div>
                 </div>
             </div>
         </div>
