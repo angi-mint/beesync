6c8e0b2100e2f5cdd8203a8b445049a37d0bc9f8
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 15:53:45 +0200

    add title
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+import { Head } from '@inertiajs/vue3';
 import HeaderInc from "@/Layouts/Header/HeaderInc.vue";
 </script>
 
 <template>
+    <Head title="Home"/>
 <HeaderInc></HeaderInc>
 </template>
 
