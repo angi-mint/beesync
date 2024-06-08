41ba2904d8d35e6410266eabfe0aceaa61890160
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-06-08 19:53:24 +0200

    added navigation translations
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- resources/js/lang/messages.ts
+++ resources/js/lang/messages.ts
@@ -3,12 +3,26 @@ export default {
         header: {
             login: 'Login',
             logout: 'Logout',
-        }
+        },
+        navigation: {
+            home: 'Home',
+            tasks: 'Tasks',
+            calendar: 'Calendar',
+            friends: 'Friends',
+            timer: 'Timer',
+        },
     },
     de : {
         header: {
             login: 'Anmelden',
             logout: 'Abmelden',
-        }
+        },
+        navigation: {
+            home: 'Startseite',
+            tasks: 'Aufgaben',
+            calendar: 'Kalender',
+            friends: 'Freunde',
+            timer: 'Timer',
+        },
     }
 }
